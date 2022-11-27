5e0831197780d605bf92d536ffb3dc4d884658d4
riverdev <yariv.rivercybe@gmail.com>
2022-11-27 11:08:15 +0200

    Refactored to support the LicenseService, it is used to demo how multiple services are annotated in the construcor

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  console.log('This is the flight-simulator app used for module tutorial.');
 }
 bootstrap();
--- src/pilot/pilot.module.ts
+++ src/pilot/pilot.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
+import { LicenseService } from './license.service';
 import { PilotController } from './pilot.controller';
 import { PilotService } from './pilot.service';
-import { Service2Service } from './service2/service2.service';
 
 @Module({
   controllers: [PilotController],
-  providers: [PilotService, Service2Service]
+  providers: [PilotService, LicenseService]
 })
 export class PilotModule {}
